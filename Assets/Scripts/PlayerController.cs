9b0ab80665a0b99fa3a05de9d5b71f111a33292e
Ronan <30617930+MWRonanD@users.noreply.github.com>
2019-02-19 01:23:13 +0100

    Tentative de gameplay

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    protected float runningSpeed = 10;
+
+    [SerializeField]
+    protected float dropSpeed = -3;
+
+
+
+    protected Rigidbody2D rig;
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rig.AddForce(new Vector2(10, 0), ForceMode2D.Force);
+            rig.velocity += 10 * Vector2.up;
+            //rig.AddForce(new Vector2(0,10), ForceMode2D.Impulse);
+        }
+        ////float hAxis = Input.GetAxis("Horizontal");
+        //float vAxis = Input.GetAxis("Vertical") <= 0 ? dropSpeed : Input.GetAxis("Vertical");
+
+        Vector2 mouvement = new Vector2(1,-1) * runningSpeed * Time.deltaTime;
+
+        rig.MovePosition(new Vector2(transform.position.x, transform.position.y) + mouvement);
+
+        //rig.velocity += 0 * Vector2.right;
+
+
+    }
+}
