4ebcc5524e74940177ddde56bf1487e663c216d7
Ronan <30617930+MWRonanD@users.noreply.github.com>
2019-02-21 22:09:11 +0100

    Lot of things
    Optimisation player, ground,
    add obstacle

---
+++ Assets/ObstacleContrioller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleContrioller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // OnCollisionEnter2D est appelé quand ce collider2D/rigidbody2D commence à toucher un autre rigidbody2D/collider2D (moteur physique 2D uniquement)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.Dead(true);
+            }
+        }
+        
+    }
+
+
+}
