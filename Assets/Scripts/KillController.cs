3c8ba35d2bc0841f39f0282e54e3145d13344b2e
Ronan <30617930+MWRonanD@users.noreply.github.com>
2019-02-24 19:04:09 +0100

    Wheels obstacle optimisation

---
+++ Assets/Scripts/KillController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillController : ObstacleController
+{
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+                player.Dead(true);
+        }
+    }
+}
